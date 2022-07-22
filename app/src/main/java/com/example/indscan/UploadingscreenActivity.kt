3c78ecacfcfe0972b401e460e8a8df6fc3b9005f
TanayK07 <tkedia7@gmail.com>
2022-07-23 02:02:28 +0530

    Maximum xml

---
+++ app/src/main/java/com/example/indscan/UploadingscreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.indscan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UploadingscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.uploadingscreen)
+    }
+}
