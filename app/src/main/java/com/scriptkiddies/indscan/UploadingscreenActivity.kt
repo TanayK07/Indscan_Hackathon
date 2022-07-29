a43fac96f73078f17ffd956bb52ba1db219ad300
entropyconquers <breakingcortana5@gmail.com>
2022-07-30 04:19:19 +0530

    Added full screen; edited colors.xml

--- app/src/main/java/com/scriptkiddies/indscan/UploadingscreenActivity.kt
+++ app/src/main/java/com/scriptkiddies/indscan/UploadingscreenActivity.kt
@@ -1,11 +1,17 @@
 package com.scriptkiddies.indscan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class UploadingscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.uploadingscreen)
     }
 }
--- app/src/main/java/com/scriptkiddies/indscan/scanningscreenActivity.kt
+++ app/src/main/java/com/scriptkiddies/indscan/scanningscreenActivity.kt
@@ -2,11 +2,17 @@ package com.scriptkiddies.indscan
 
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import net.vishesh.scanner.presentation.BaseScannerActivity
 
 class scanningscreenActivity : BaseScannerActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.scanningscreen)
     }
 
