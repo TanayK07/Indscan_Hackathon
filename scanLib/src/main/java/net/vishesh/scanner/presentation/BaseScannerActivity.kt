01ac7e2832eecce6436b48509fa063edf36d8fe0
entropyconquers <breakingcortana5@gmail.com>
2022-08-02 08:19:26 +0530

    added messageView

--- scanLib/src/main/java/net/vishesh/scanner/presentation/BaseScannerActivity.kt
+++ scanLib/src/main/java/net/vishesh/scanner/presentation/BaseScannerActivity.kt
@@ -1,5 +1,6 @@
 package net.vishesh.scanner.presentation
 
+import android.animation.LayoutTransition
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.ActivityInfo
@@ -55,9 +56,12 @@ abstract class BaseScannerActivity : AppCompatActivity() {
         binding = ActivityScannerBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        binding.root.getLayoutTransition()
+            .enableTransitionType(LayoutTransition.CHANGING);
+
 
         val viewModel: ScannerViewModel by viewModels()
-
+        var message = binding.message
         viewModel.isBusy.observe(this, { isBusy ->
             binding.progress.visibility = if (isBusy) {
                 View.VISIBLE
@@ -91,7 +95,139 @@ abstract class BaseScannerActivity : AppCompatActivity() {
             onError(it)
             Log.e(ScannerActivity::class.java.simpleName, it.message, it)
         })
+        viewModel.mlCorners.observe(this) {
+            var counter = 0;
+            var lastPropChange = R.string.biv
+            Log.d(ScannerActivity::class.java.simpleName, "mlCorners: ${getResources().getString(lastPropChange)}")
+            it?.let { cornersX ->
 
+                viewModel.corners.observe(this) {
+                    it?.let { corners ->
+                        message.visibility = View.VISIBLE
+                        if(corners.calculateSize().width > 0.7 * binding.root.width) {
+                            if(lastPropChange != R.string.farther) {
+                                counter = 1
+                                lastPropChange = R.string.farther
+                            }
+                            else {
+                                counter++
+                            }
+                            if(counter > 5) {
+                                message.setText(R.string.farther)
+                                lastPropChange = R.string.farther
+                                counter = 0
+                            }
+
+                        }
+                        else if(corners.calculateSize().width < 0.4 * binding.root.width) {
+                            if(lastPropChange != R.string.closer) {
+                                counter = 1
+                                lastPropChange = R.string.closer
+                            }
+                            else {
+                                counter++
+                            }
+                            if(counter > 5) {
+                                message.setText(R.string.closer)
+                                lastPropChange = R.string.closer
+                                counter = 0
+                            }
+
+                        }
+                        else{
+                            if(corners.isTooSteep()){
+                                if(lastPropChange != R.string.angle) {
+                                    counter = 1
+                                    lastPropChange = R.string.angle
+                                }
+                                else {
+                                    counter++
+                                }
+                                if(counter > 5) {
+                                    message.setText(R.string.angle)
+                                    lastPropChange = R.string.angle
+                                    counter = 0
+                                }
+                            }
+                            else{
+                                if(lastPropChange != R.string.ready) {
+                                    counter = 1
+                                    lastPropChange = R.string.ready
+                                }
+                                else {
+                                    counter++
+                                }
+                                if(counter > 5) {
+                                    message.setText(R.string.ready)
+                                    lastPropChange = R.string.ready
+                                    counter = 0
+                                }
+
+                            }
+
+                        }
+                    } ?: run {
+                        message.visibility = View.VISIBLE
+                        if(cornersX.calculateSize().width > 0.8 * binding.root.width) {
+                            if (lastPropChange != R.string.farther) {
+                                counter = 1
+                                lastPropChange = R.string.farther
+
+                            } else {
+                                counter++
+                            }
+                            if (counter > 10) {
+                                message.setText(R.string.farther)
+                                lastPropChange = R.string.farther
+                                counter = 0
+                            }
+
+                        }
+                        else if(cornersX.calculateSize().width < 0.4 * binding.root.width) {
+                            if (lastPropChange != R.string.closer) {
+                                counter = 1
+                                lastPropChange = R.string.closer
+                            } else {
+                                counter++
+                            }
+                            if (counter > 10) {
+                                message.setText(R.string.closer)
+                                lastPropChange = R.string.closer
+                                counter = 0
+                            }
+
+                        }
+                        else{
+                            if (lastPropChange != R.string.finding) {
+                                counter = 1
+                                lastPropChange = R.string.finding
+                            } else {
+                                counter++
+                            }
+                            if (counter > 10) {
+                                message.setText(R.string.finding)
+                                lastPropChange = R.string.finding
+                                counter = 0
+                            }
+                            //message.visibility = View.INVISIBLE
+                        }
+
+                    }
+                }
+            } ?: run {
+                if (lastPropChange != R.string.dnf) {
+                    counter = 1
+                    lastPropChange = R.string.dnf
+                } else {
+                    counter++
+                }
+                if (counter > 10) {
+                    message.setText(R.string.dnf)
+                    lastPropChange = R.string.dnf
+                    counter = 0
+                }
+            }
+        }
         viewModel.corners.observe(this) {
             it?.let { corners ->
                 binding.hud.onCornersDetected(corners)
